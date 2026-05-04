43bd52f49f7274cddfc864de5edb7080042dce96
EskandarAtrakchi <isgan151@gmail.com>
2026-05-04 16:00:06 +0100

    Add documentation comment to ThemeProvider component for clarity

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -6,6 +6,7 @@ import {
   type ThemeProviderProps,
 } from 'next-themes'
 
+// The ThemeProvider component is a wrapper around the NextThemesProvider from the next-themes library. It allows us to manage and provide theme-related context to the entire application. By using this component.
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
